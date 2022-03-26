3ac01634e8c48761db73000069d7f23d2ec55768
Eston <liyuelin2004@gmail.com>
2022-03-26 18:12:44 -0400

    function that adds JWT token to a csv file

---
+++ CSVConverter.ts
@@ -0,0 +1,47 @@
+require('dotenv').config();
+import fs from 'fs';
+import readline from 'readline';
+import jwt from 'jsonwebtoken';
+
+const createNewCSV = async (fileName: fs.PathLike) => {
+    const entryObjs = [];
+    const fileStream = fs.createReadStream(fileName);
+
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
+
+    for await (const line of rl) {
+        const entryData = line.split(',');
+        entryData.push(generateJWT(entryData[0], entryData[1], entryData[2]));
+        entryObjs.push(entryData.join(','));
+    }
+
+    outputToCSV(entryObjs.join("\n"));
+};
+
+const outputToCSV = (data: string) => {
+    fs.writeFile("entryWithJWT.csv", data, (err) => {
+        if (err)
+            console.log(err);
+        else {
+            console.log("File written successfully\n");
+        }
+    });
+};
+
+const generateJWT = (firstName: String, lastName: String, email: String) => {
+    return jwt.sign({
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "5000h",
+        }
+    );
+};
+
+
